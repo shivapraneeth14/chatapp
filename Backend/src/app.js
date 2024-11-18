b7a36685b5a0f1960ef10115adebe3d279ec8f9e
shivapraneeth14 <shivakandala43@gmail.com>
2024-11-18 22:51:34 +0530

    changes done

--- Backend/src/app.js
+++ Backend/src/app.js
@@ -4,7 +4,7 @@ import cookieParser from "cookie-parser"
 
 const app = express();
 
-app.use(cors({ origin: 'http://localhost:5173', credentials: true }));
+app.use(cors({ origin: `${process.env.BACKEND_URL}`, credentials: true }));
 app.use(express.json({limit: '50mb'}))
 app.use(express.urlencoded({extended:true,
     limit: '50mb'
